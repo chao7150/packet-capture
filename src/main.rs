fa1ef3827f52c4974395194cefb582432ba9b352
chao7150 <22640673+chao7150@users.noreply.github.com>
2025-04-22 23:45:14 +0900

    生のイーサネットフレームが見える

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use pnet::datalink;
 
 fn main() {
-    let interface = datalink::interfaces()
-        .into_iter()
+    use pnet::datalink::Channel::Ethernet;
+
+    let binding = datalink::interfaces();
+    let interface = binding
+        .iter()
         .filter(|x| x.is_up())
         .filter(|x| !x.is_loopback())
         .next()
         .expect("no up interface");
     println!("{:?}", interface);
+
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("unknown channel type"),
+        Err(_) => panic!("failed to open channel"),
+    };
+    loop {
+        println!("{:?}", rx.next());
+    }
 }
